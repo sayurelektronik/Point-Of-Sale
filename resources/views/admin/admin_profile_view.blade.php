db58524dbcc29ede7d9b9a3ee0237155bb666f1f
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2023-12-18 02:53:31 +0700

    Admin Profile & Image Update part 3

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,6 +1,8 @@
 @extends('admin_dashboard')
 
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 <div class="content">
 
     <!-- Start Content-->
@@ -101,14 +103,14 @@
                                     <div class="col-md-12">
                                         <div class="mb-3">
                                             <label for="example-fileinput" class="form-label">Admin Profile Image</label>
-                                            <input type="file" id="example-fileinput" class="form-control">
+                                            <input type="file" name="photo" id="image" class="form-control">
                                         </div>
                                     </div> <!-- end col -->
                                     <div class="col-md-12">
                                         <div class="mb-3">
                                             <label for="example-fileinput" class="form-label"></label>
-                                            <img src="{{ (!empty($adminData->photo)) ? url('upload/admin_image/'.$adminData->photo) : url('upload/no_image.jpg') }}"
-                            class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
+                                            <img id="showImage" src="{{ (!empty($adminData->photo)) ? url('upload/admin_image/'.$adminData->photo) : url('upload/no_image.jpg') }}"
+                                             class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
                                         </div>
                                     </div> <!-- end col -->
                                 </div> <!-- end row -->
@@ -129,4 +131,18 @@
     </div> <!-- container -->
 
 </div> <!-- content -->
+
+<script type="text/javascript">
+
+	$(document).ready(function(){
+		$('#image').change(function(e){
+			var reader = new FileReader();
+			reader.onload =  function(e){
+				$('#showImage').attr('src',e.target.result);
+			}
+			reader.readAsDataURL(e.target.files['0']);
+		});
+	});
+</script>
+
 @endsection

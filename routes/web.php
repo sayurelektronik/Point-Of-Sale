4390415ef8c8696c6377ce29f6c2581ddadba519
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2023-12-17 09:37:09 +0700

    admin logout option

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');

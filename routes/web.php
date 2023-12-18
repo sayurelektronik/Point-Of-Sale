812e7a681fca99b653ea25ac6a956f77a8fe06ff
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2023-12-18 21:27:30 +0700

    Protect URL By Middleware

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,11 @@ require __DIR__.'/auth.php';
 
 Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
 Route::get('/logout', [AdminController::class, 'AdminLogoutPage'])->name('admin.logout.page');
-Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
-Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
-Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+
+// auth 
+Route::middleware(['auth'])->group(function(){
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
+    Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+});

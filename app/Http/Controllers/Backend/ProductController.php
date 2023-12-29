f110fcbd21a21d2556fe0a83b42f2e15025d615b
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2023-12-29 14:26:59 +0700

    Product Management System Part 2

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Supplier;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
@@ -15,5 +17,13 @@ class ProductController extends Controller
         $product = Product::latest()->get();
 
         return view('backend.product.all_product', compact('product'));
+    }
+
+    public function AddProduct() {
+
+        $supplier = Supplier::latest()->get();
+        $category = Category::latest()->get();
+
+        return view('backend.product.add_product', compact('supplier', 'category'));
     }
 }

5f8d2d54c18ad449de74013eb86e7e18ee49bfc6
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2024-01-05 04:07:44 +0700

    Create Order System Part 3

--- resources/views/backend/invoice/product_invoice.blade.php
+++ resources/views/backend/invoice/product_invoice.blade.php
@@ -88,30 +88,27 @@
                                         <tr><th>#</th>
                                             <th>Item</th>
                                             <th style="width: 10%">Jumlah</th>
-                                            <th style="width: 10%">Hours Rate</th>
+                                            <th style="width: 10%">Unit Cost</th>
                                             <th style="width: 10%" class="text-end">Total</th>
                                         </tr></thead>
                                         <tbody>
+
+                                        @php
+                                            $sl = 1;
+                                        @endphp
+
+                                        @foreach($contents as $key=> $item)
                                         <tr>
-                                            <td>1</td>
+                                            <td>{{ $sl++ }}</td>
                                             <td>
-                                                <b>Web Design</b> <br/>
-                                                2 Pages static website - my website
+                                                <b>{{ $item->name }}</b> <br/>
+
                                             </td>
-                                            <td>22</td>
-                                            <td>$30</td>
-                                            <td class="text-end">$660.00</td>
+                                            <td>{{ $item->qty }}</td>
+                                            <td>Rp. {{ $item->price }}</td>
+                                            <td class="text-end">Rp. {{ $item->price*$item->qty }}</td>
                                         </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>
-                                                <b>Software Development</b> <br/>
-                                                Invoice editor software - AB'c Software
-                                            </td>
-                                            <td>112.5</td>
-                                            <td>$35</td>
-                                            <td class="text-end">$3937.50</td>
-                                        </tr>
+                                        @endforeach
 
                                         </tbody>
                                     </table>
@@ -124,21 +121,13 @@
                             <div class="col-sm-6">
                                 <div class="clearfix pt-5">
                                     <h6 class="text-muted">Notes:</h6>
-
-                                    <small class="text-muted">
-                                        All accounts are to be paid within 7 days from receipt of
-                                        invoice. To be paid by cheque or credit card or direct payment
-                                        online. If account is not paid within 7 days the credits details
-                                        supplied as confirmation of work undertaken will be charged the
-                                        agreed quoted fee noted above.
-                                    </small>
                                 </div>
                             </div> <!-- end col -->
                             <div class="col-sm-6">
                                 <div class="float-end">
-                                    <p><b>Sub-total:</b> <span class="float-end">$4597.50</span></p>
-                                    <p><b>Discount (10%):</b> <span class="float-end"> &nbsp;&nbsp;&nbsp; $459.75</span></p>
-                                    <h3>$4137.75 USD</h3>
+                                    <p><b>Sub-total:</b> <span class="float-end">Rp. {{ Cart::subtotal() }}</span></p>
+                                    <p><b>Vat (21%):</b> <span class="float-end"> &nbsp;&nbsp;&nbsp; Rp. {{ Cart::tax() }}</span></p>
+                                    <h3>Rp. {{ Cart::total() }}</h3>
                                 </div>
                                 <div class="clearfix"></div>
                             </div> <!-- end col -->
@@ -148,7 +137,7 @@
                         <div class="mt-4 mb-1">
                             <div class="text-end d-print-none">
                                 <a href="javascript:window.print()" class="btn btn-primary waves-effect waves-light"><i class="mdi mdi-printer me-1"></i> Print</a>
-                                <a href="#" class="btn btn-info waves-effect waves-light">Submit</a>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signup-modal">Create Invoice </button>
                             </div>
                         </div>
                     </div>
@@ -157,68 +146,60 @@
         </div>
         <!-- end row -->
 
-        <div id="signup-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-
-                    <div class="modal-body">
-                        <div class="text-center mt-2 mb-4">
-                            <div class="auth-logo">
-                                <a href="index.html" class="logo logo-dark">
-                                    <span class="logo-lg">
-                                        <img src="assets/images/logo-dark.png" alt="" height="24">
-                                    </span>
-                                </a>
-
-                                <a href="index.html" class="logo logo-light">
-                                    <span class="logo-lg">
-                                        <img src="assets/images/logo-light.png" alt="" height="24">
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-
-                        <form class="px-3" action="{{ url('/final-invoice') }}" method="POST">
-                            @csrf
-
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Payment</label>
-                                <select name="payment_status" id="example-select" class="form-select"></select>
-                                    <option value="HandCash">HandCash</option>
-                                    <option value="Cheque">Cheque</option>
-                                    <option value="Due">Due</option>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Pay Now</label>
-                                <input class="form-control" type="text" name="pay" placeholder="Pay Now">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Due Amount</label>
-                                <input class="form-control" type="text" name="due" placeholder="Due Amount">
-                            </div>
-
-                            <input type="hidden" name="customer_id" value="{{ $customer->id }}">
-                            <input type="hidden" name="order_date" value="{{ date('d-F-Y') }}">
-                            <input type="hidden" name="order_status" value="Pending">
-                            <input type="hidden" name="total_products" value="{{ Cart::count() }}">
-                            <input type="hidden" name="sub_total" value="{{ Cart::subtotal() }}">
-                            <input type="hidden" name="vat" value="{{ Cart::tax() }}">
-                            <input type="hidden" name="total" value="{{ Cart::total() }}">
-
-                            <div class="mb-3 text-center">
-                                <button class="btn btn-primary" type="submit">Complete Order</button>
-                            </div>
-
-                        </form>
-
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
-
     </div> <!-- container -->
 
 </div> <!-- content -->
+
+<div id="signup-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-body">
+                <div class="text-center mt-2 mb-4">
+                    <div class="auth-logo">
+                        <h3>Invoice Of {{ $customer->name }}</h3>
+            			<h3>Jumlah Total Rp. {{ Cart::total() }}</h3>
+                    </div>
+                </div>
+
+                <form class="px-3" action="{{ url('/final-invoice') }}" method="POST">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Payment</label>
+                        <select name="payment_status" id="example-select" class="form-select">
+                            <option value="UangTunai">Uang Tunai</option>
+                            <option value="Cheque">Cek</option>
+                            <option value="Due">Jatuh Tempo</option>
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Bayar Sekarang</label>
+                        <input class="form-control" type="text" name="pay" placeholder="Bayar Sekarang">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Jumlah Jatuh Tempo</label>
+                        <input class="form-control" type="text" name="due" placeholder="Jumlah Jatuh Tempo">
+                    </div>
+
+                    <input type="hidden" name="customer_id" value="{{ $customer->id }}">
+                    <input type="hidden" name="order_date" value="{{ date('d-F-Y') }}">
+                    <input type="hidden" name="order_status" value="Pending">
+                    <input type="hidden" name="total_products" value="{{ Cart::count() }}">
+                    <input type="hidden" name="sub_total" value="{{ Cart::subtotal() }}">
+                    <input type="hidden" name="vat" value="{{ Cart::tax() }}">
+                    <input type="hidden" name="total" value="{{ Cart::total() }}">
+
+                    <div class="mb-3 text-center">
+                        <button class="btn btn-primary" type="submit">Complete Order</button>
+                    </div>
+
+                </form>
+
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 @endsection

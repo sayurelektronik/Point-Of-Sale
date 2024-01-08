ae33b8c98579eecd3139e0f4ff64ae21c79b964f
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2024-01-08 13:25:21 +0700

    Setup User Permission Part 3

--- app/Http/Controllers/Backend/RoleController.php
+++ app/Http/Controllers/Backend/RoleController.php
@@ -34,4 +34,42 @@ class RoleController extends Controller
 
         return redirect()->route('all.permission')->with($notification);
     }
+
+    public function EditPermission($id){
+
+        $permission = Permission::findOrFail($id);
+        return view('backend.pages.permission.edit_permission',compact('permission'));
+    }
+
+
+    public function UpdatePermission(Request $request){
+
+        $per_id = $request->id;
+
+        Permission::findOrFail($per_id)->update([
+            'name' => $request->name,
+            'group_name' => $request->group_name,
+
+        ]);
+
+        $notification = array(
+            'message' => 'Permission Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.permission')->with($notification);
+    }
+
+
+    public function DeletePermission($id){
+
+        Permission::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Permission Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
 }
